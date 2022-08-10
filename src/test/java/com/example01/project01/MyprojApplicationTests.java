a37c41689ddb148de6f09f87b8181e774c0f25cd
Gowtham M <1000073407@hexaware.com>
2022-08-10 14:36:42 +0530

    files added

---
+++ src/test/java/com/example01/project01/MyprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example01.project01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
